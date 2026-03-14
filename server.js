2cbd592af6e3a12651f9c2738bcce3e35107e230
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-03-14 23:18:54 +0300

    add course polcy

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ process.on("uncaughtException", (err) => {
 });
 
 const app = express();
-app.use(cors());
+
 app.use(bodyParser.json());
+const cors = require('cors');
+// Enable CORS for frontend origin
+app.use(cors({
+  origin: 'https://mfn-nu.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 const authRoutes = require("./routes/auth");
 const inviteRoutes = require("./routes/invite");
